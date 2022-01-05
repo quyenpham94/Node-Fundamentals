71a091a5e880c0cf9ceffec555ca7644a57d47f4
Quyen Pham <hoangquyen174@gmail.com>
2022-01-05 11:13:03 -0600

    update sqlalchemy to host on heroku

--- callbacks-exercise/part2/app.js
+++ callbacks-exercise/part2/app.js
@@ -7,5 +7,18 @@ $.getJSON(`${baseURL}/new/draw/`, function(data) {
     console.log(`${value.toLowerCase()} of ${suit.toLowerCase()}`);
 });
 
+// 2.
+$.getJSON(`${baseURL}/new/draw/`, function(data) {
+    let firstCard = data.cards[0];
+    let deckId = data.deck_id;
+    $.getJSON(`${baseURL}/${deckId}/draw/`, function(data) {
+        let secondCard = data.cards[0];
+        [firstCard, secondCard].forEach(function(card) {
+            console.log(`${card.value.toLowerCase()} of ${card.suit.toLowerCase()}`);
+        });
+    });
+});
+
+
 
 });
