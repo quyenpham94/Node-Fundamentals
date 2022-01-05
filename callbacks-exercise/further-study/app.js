7c79a7eee6be479f41a3e06f39e28164ce676223
Quyen Pham <hoangquyen174@gmail.com>
2022-01-05 11:37:49 -0600

    update callbacks exercises

--- callbacks-exercise/further-study/app.js
+++ callbacks-exercise/further-study/app.js
@@ -34,7 +34,7 @@ $(function() {
                let name = data.name;
                $.getJSON(data.species.url, function(data) {
                    let descriptionObj = data.flavor_text_entries.find(
-                       entry => entry.language.name == "en"
+                       entry => entry.language.name === "en"
                    );
                    let description = descriptionObj ? descriptionObj.flavor_text : "No description available.";
                    console.log(`${name}: ${description}`);
@@ -45,5 +45,39 @@ $(function() {
     
     // 4.
     let $btn = $("button");
-    let 
+    let $pokeArea = $("#pokemon-area");
+
+    $btn.on("click", function() {
+        $pokeArea.empty();
+        $.getJSON(`${baseURL}/pokemon/?limit=1000`, function(data) {
+            let randomPokemonUrls = [];
+            for (let i = 0; i < 3; i++) {
+                let randomIdx = Math.floor(Math.random() * data.results.length);
+                let url = data.results.splice(randomIdx, 1)[0].url;
+                randomPokemonUrls.push(url);
+            }
+            randomPokemonUrls.forEach(function(url, i) {
+                $.getJSON(url, function(data) {
+                    let name = data.name;
+                    let imgSrc = data.sprites.front_default;
+                    $.getJSON(data.species.url, function(data) {
+                        let descriptionObj = data.flavor_text_entries.find(
+                            entry => entry.language.name === "en"
+                        );
+                        let description = descriptionObj ? descriptionObj.flavor_text : "No description available.";
+                        $pokeArea.append(makePokeCard(name, imgSrc, description));
+                    });
+                });
+            });
+        });
+    });
+    function makePokeCard(name, imgSrc, description) {
+        return `
+        <div class="card">
+            <h1>${name}</h1>
+            <img src=${imgSrc} />
+            <p>${description}</p>
+        </div>
+         `
+    }
 })
